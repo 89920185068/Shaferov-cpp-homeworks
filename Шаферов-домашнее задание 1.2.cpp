8dc3bc404213812c126ca0497f2feacd1530de97
89920185068 <shaferovnick@gmail.com>
2026-01-03 16:43:18 +0500

    Add files via upload

---
+++ Шаферов-домашнее задание 1.2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string name;
+	
+	
+	// Запрашиваем имя пользователя
+	std::cout <<"Введите ваше имя: ";
+	std::cin >> name;
+	
+	// Выводим персональное приветсвие
+	std::cout << "Привет, " << name << "Рад вас видеть!" << std::endl;
+	
+	return 0;
+}
